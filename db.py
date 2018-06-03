9c29fa1656c33ec7aca80b2710de2b7a47d022d9
schrooms <schrooms@gmail.com>
2018-06-03 13:21:14 +0200

    added comments

--- db.py
+++ db.py
@@ -3,9 +3,11 @@ import datetime
 
 
 def write(temp):
+    '''
+    writes the temp to a csv file with date colomn
+    '''
     t = datetime.datetime.now()
     with open('data.csv', 'a') as csvfile:
-
         fieldnames = ['date', 'temp']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow({'date': t.strftime('%Y-%m-%d %H:%M:%S'), 'temp': temp})
